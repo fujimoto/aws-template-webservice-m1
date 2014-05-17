b79e8bd2e8758fb4f6db0251cba561b40d14c709
Masaki Fujimoto <masaki.fujimoto@gree.net>
2014-05-18 02:49:57 +0900

    sync up with new aws-template

--- lib/aws/template/webservice_m1.rb
+++ lib/aws/template/webservice_m1.rb
@@ -1,11 +1,61 @@
 require "aws/template/webservice_m1/version"
 
-module Aws
-  module Template
-    module Webservice
-      module M1
-        # Your code goes here...
+module AWS
+  class Template
+    class WebserviceM1 < Template
+      def initialize(override_path)
+        super
+        @template_path = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "..", "config", "aws-template.json"))
       end
+
+      protected
+      def execute(config, *args)
+        vpc.create(config["vpc_name"], config["vpc_cidr"])
+        config["vpc_id"] = vpc.aws_instance.id
+
+        subnet.create(vpc.aws_instance, config["subnets"])
+        # create cidr -> subnet_id maps (for vagrant)
+        config["subnets"]["cidr"] = {}
+        subnet.aws_instance.each do |s|
+          config["subnets"]["cidr"][s.cidr_block] = s.id
+        end
+
+        igw.create(vpc.aws_instance, config["igw_name"])
+        config["igw_id"] = igw.aws_instance.id
+
+        route.create(vpc.aws_instance, igw.aws_instance, config["subnets"])
+
+        security_group.create(vpc.aws_instance, config["security_groups"], config["security_groups_ssh_source"])
+        security_group.aws_instance.each do |name, s|
+          config["security_groups"][name]["id"] = s.id
+        end
+
+        key_pair.create(config["key_pair_name"], File.expand_path(config["key_pair_local_path"]))
+
+        load_balancer.create(config["load_balancers"], config["subnets"], config["security_groups"])
+        load_balancer.aws_instance.each do |lb_name, lb|
+          config["load_balancers"][lb_name]["dns_name"] = lb.dns_name
+        end
+
+        # temorary update for lauching instance
+        store(config, @customized_path)
+
+        launch_proc = args.shift
+        ec2.create(config["ec2_gateway_name"], launch_proc)
+        eni = ec2.eni(config["ec2_gateway_name"])
+        if eni.nil?
+          log("warning: no ENIs in gateway instance, skip creating routes")
+        end
+
+        route.associate(vpc.aws_instance, eni, "route-main", "0.0.0.0/0")
+
+        r53.create(config["zone_name"])
+        r53.add_a_record(config["ec2_gateway_zone_name"], eni.elastic_ip.public_ip)
+        config["load_balancers"].each do |lb_name, lb_config|
+          r53.add_a_record_lb(load_balancer.aws_instance, lb_name, lb_config)
+        end
+      end
+
     end
   end
 end
