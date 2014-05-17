b6dc600f0f8fab1705f614afac5d355aae5dbd32
Masaki Fujimoto <masaki.fujimoto@gree.net>
2014-05-17 15:00:01 +0900

    initial

---
+++ lib/aws/template/webservice/m1.rb
@@ -0,0 +1,11 @@
+require "aws/template/webservice/m1/version"
+
+module Aws
+  module Template
+    module Webservice
+      module M1
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/aws/template/webservice/m1/version.rb
@@ -0,0 +1,9 @@
+module Aws
+  module Template
+    module Webservice
+      module M1
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
